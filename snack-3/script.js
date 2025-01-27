8054cc733ea3d38e9d927c7abfe3bd38b816c0b6
giuseppezaccato <giuseppezaccato@gmail.com>
2025-01-27 17:13:28 +0100

    ADD: 1,2,3

--- snack-3/script.js
+++ snack-3/script.js
@@ -2,3 +2,16 @@ const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
+
+//task map ==>> crea in automatico un ciclo forEach con istruzioni nuovoArray.push(element +1)
+const pari = numbers.map((element) => element + 1);
+console.log(pari);
+
+//task senza map avrei dovuto fare un for con dentro un push manualmente!
+const pariA = [];
+for (let i = 0; i < numbers.length; i++) {
+    pariA.push(numbers[i] + 1)
+};
+console.log(pariA);
+
+
